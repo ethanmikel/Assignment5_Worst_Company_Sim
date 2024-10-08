4a46d007828f5ac4f0ef5cc02b21b94fb55609be
Ethan Mikel <54323448+ethanmikel@users.noreply.github.com>
2024-10-08 18:00:04 -0500

    moving the self.salary and happiness

--- employees.py
+++ employees.py
@@ -189,11 +189,11 @@ class TemporaryEmployee(Employee):
                     self.performance >= TEMP_EMPLOYEE_PERFORMANCE_THRESHOLD:
                 self.savings += MANAGER_BONUS
             else:
-                if self.salary == 0:
-                    self.is_employed = False
                 self.salary //= 2
                 self.happiness -= 5
-
+                if self.salary == 0:
+                    self.is_employed = False
+                
 class PermanentEmployee(Employee):
     """
     A subclass of Employee representing a permanent employee.
